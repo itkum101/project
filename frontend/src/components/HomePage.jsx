f19ce13f82eb59a7494ee3c97ef70448a10589d1
Mukti Subedi <077bct048.mukti@pcampus.edu.np>
2025-02-22 06:09:01 +0545

    Optimized Backend Dockerfile

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <section className="home" id="home">
       <div className="home-content">
-        <h1 className="home-title">!!!!Welcome to De-LlMIT</h1>
+        <h1 className="home-title">!!!!Welcome to De-LlMIT!!!!</h1>
         <p className="home-tagline">
           Decentralized Large-Language Model Inference and Fine-Tuning. 
           Empowering AI with your devices!
