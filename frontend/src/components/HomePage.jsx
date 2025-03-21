cd9807ec207f85552d2f9fd4b960116931248678
Mukti Subedi <077bct048.mukti@pcampus.edu.np>
2025-03-21 13:31:18 +0545

    Code changes

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -5,12 +5,12 @@ const HomePage = () => {
   return (
     <section className="home" id="home">
       <div className="home-content">
-        <h1 className="home-title">Welcome to De-LlMIT!!</h1>
+        <h1 className="home-title"> VEry Very Welcome to De-LlMIT!!</h1>
         <p className="home-tagline">
           Decentralized Large-Language Model Inference and Fine-Tuning. 
-          Empowering AI with own  !!. Let's see. 
+          Empowering AI with own yes yes !!. 
         </p>
-        <button className="cta-button">Explore Features yes</button>
+        <button className="cta-button">Explore Features yes </button>
       </div>
     </section>
   );
