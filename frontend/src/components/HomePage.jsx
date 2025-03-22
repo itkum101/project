91c37c71366a1b4ebbbc4d5862d9a0ca9357939e
Mukti Subedi <077bct048.mukti@pcampus.edu.np>
2025-03-22 10:47:39 +0545

    Code changes

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
       <div className="home-content">
         <h1 className="home-title">  Welcome to De-LlMIT!!</h1>
         <p className="home-tagline">
-          Decentralized LLM Inference and Fine-Tuning. 
+          Decentralized Large LM Inference and Fine-Tuning. 
           Empowering AI with own edge device !!. 
         </p>
         <button className="cta-button">Explore Features   </button>
